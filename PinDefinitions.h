1f11efcbe8a349e80a4464f05607e2f7da58efc3
specospec <vinuvnair@outlook.com>
2016-06-08 17:02:35 +0200

    added PMOD sensor pins

--- PinDefinitions.h
+++ PinDefinitions.h
@@ -3,8 +3,16 @@
 #define PINDEFINITIONS_H_
 
 
-#define echoPin 7 // Echo Pin
-#define trigPin 8 // Trigger Pin
-#define LEDPin 13 // Onboard LED
+#define ECHOPIN 7 // Echo Pin
+#define TRIGPIN 8 // Trigger Pin
+//#define LEDPIN 13 // Onboard LED
+
+#define PWPIN1 9 // for PMOD sensors : PWM input
+#define PWPIN2 5
+#define PWPIN3 6
+
+#define TRIGGERPIN1 13 	// for PMOD sensors : Trgger
+#define TRIGGERPIN2 12
+#define TRIGGERPIN3 11
 
 #endif
