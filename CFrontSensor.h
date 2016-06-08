6c2092c8206640b42c212b1475574b8ec20a3221
specospec <vinuvnair@outlook.com>
2016-06-08 16:58:55 +0200

    read and pin init methods addded

--- CFrontSensor.h
+++ CFrontSensor.h
@@ -7,11 +7,16 @@
 
 #ifndef CFRONTSENSOR_H_
 #define CFRONTSENSOR_H_
-
+#include "Arduino.h"
 class CFrontSensor {
 public:
 	CFrontSensor();
+	void Pin_init(unsigned char PWpinnum,unsigned char TRIGpinnum);
 	virtual ~CFrontSensor();
+	int read_sensor();
+private:
+	unsigned char	m_PWpinNumber;
+	unsigned char	m_TRIGpinNumber;
 };
 
 #endif /* CFRONTSENSOR_H_ */
