fe153b094d7202e2d838190c6a88e554c145345f
specospec <vinuvnair@outlook.com>
2016-06-08 17:00:32 +0200

    serial print of sensors and integration

--- droneUpdated.cpp
+++ droneUpdated.cpp
@@ -1,24 +1,49 @@
 // Do not remove the include below
 #include "droneUpdated.h"
-#include "CMAV.h"
 
-
+CFrontSensor PMOD1;
+CFrontSensor PMOD2;
+CFrontSensor PMOD3;
 //The setup function is called once at startup of the sketch
 void setup()
 {
 // Add your initialization code here
-	Serial.begin(57600);
+	// Serial port inits
+	Serial.begin(9600);
+		// Sensors pin inits
+	PMOD1.Pin_init(PWPIN1,TRIGGERPIN1);
+	PMOD2.Pin_init(PWPIN2,TRIGGERPIN2);
+	PMOD3.Pin_init(PWPIN3,TRIGGERPIN3);
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
 //Initialize all control variables
-CMAV MAVlink;
+//CMAV MAVlink;
+int sensor1, sensor2, sensor3;
+
+sensor1=PMOD1.read_sensor();
+sensor2=PMOD2.read_sensor();
+sensor3=PMOD3.read_sensor();
+
+Serial.print("S1");
+Serial.print(" : ");
+Serial.print(sensor1);
+Serial.print(" ");
+Serial.print("S2");
+Serial.print(" : ");
+Serial.print(sensor2);
+Serial.print(" ");
+Serial.print("S3");
+Serial.print(" : ");
+Serial.print(sensor3);
+Serial.print(" ");
+Serial.println(" ");
 
 // Update Sensors and control variables periodically
-MAVlink.sent_HeartBeat();
-MAVlink.comm_receive();
+//MAVlink.sent_HeartBeat();
+//MAVlink.comm_receive();
 
 // Process the input data , create events eg height exceeded threshold , drone too close to object etc
 
--- droneUpdated.h
+++ droneUpdated.h
@@ -8,6 +8,9 @@
 #define _droneUpdated_H_
 #include "Arduino.h"
 #include "mavlink.h"        // Mavlink interface
+#include "PinDefinitions.h"
+#include "CMAV.h"
+#include "CFrontSensor.h"
 
 enum H_DRONE_MODE {DRONE_INIT,DRONE_FLIGHT_INIT,DRONE_FLIGHT_HORIZONTAL,DRONE_HOLD_POSITION, DRONE_FLIGHT_VERTICAL,DRONE_COLLITION_DETECT,DRONE_RETURN_HOME};
 
